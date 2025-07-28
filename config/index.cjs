955d9d78080f0c04282055b870d4abe489340750
gengqq <qingqi_520@163.com>
2025-07-28 14:16:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -77,6 +77,36 @@ const USER_CONFIG = {
         { keyword: 'marry_day', date: '2099-09-09' },
       ],
     },
+    {
+      // 想要发送的人的名字
+      name: '宝贝',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oYoimvrdJxSFW7cVx4VS60n9Sbn8',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'e-At90meTPuA5B0Wad0C0I1esiIa8WaOQbKml0otzpI',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '02-25',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '1992', date: '02-25',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '宝贝', year: '1992', date: '01-21',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2025', date: '05-03',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2025-07-01' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2099-09-09' },
+      ],
+    },
   ],
 
 
