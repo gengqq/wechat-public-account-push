d2c16d6e9f82425c53fe804a0b29bcb85cdfa344
gengqq <qingqi_520@163.com>
2025-07-28 11:09:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oYoimvm8CmWEZj29Q4F_78qouCSE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Wue80GrtY1nAia4tam-FDxZF9WNx9pwcBc0LMpAN1u8',
+      useTemplateId: 'Wue80GrtY1nAia4tam4FDxZF9WNx9pwcBc0LMpAN1u8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-25',
       festivals: [
